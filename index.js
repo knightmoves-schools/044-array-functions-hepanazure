b5f7277b81835c1bd03a8dce5b32964992116f5b
hepanazure <hepan.azure@students.msswarriors.org>
2026-05-01 18:36:00 +0000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let automobiles = ['car', 'truck', 'motorcycle'];
 
+automobiles.push('airplane', 'skateboard');
+automobiles.unshift('bike', 'helicopter');
+
+let automobilesCopy = `automobiles: ${automobiles}`;
+
+automobiles.pop();
+automobiles.shift();
+
+document.getElementById('result').innerHTML = automobiles;
